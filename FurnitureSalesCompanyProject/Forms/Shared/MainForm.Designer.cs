e09bda7d233bbf03ce1fb1deee40eaaf5e305015
Maksim <59286805+Maflend@users.noreply.github.com>
2021-11-10 00:35:20 +0300

    Add start text on mainmenu

--- FurnitureSalesCompanyProject/Forms/Shared/MainForm.Designer.cs
+++ FurnitureSalesCompanyProject/Forms/Shared/MainForm.Designer.cs
@@ -37,7 +37,10 @@ namespace FurnitureSalesCompanyProject.Forms
             this.btnOpenFurnituresForm = new System.Windows.Forms.Button();
             this.btnOpenCustomersForm = new System.Windows.Forms.Button();
             this.panelContent = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelMenuTab.SuspendLayout();
+            this.panelContent.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenuTab
@@ -153,11 +156,37 @@ namespace FurnitureSalesCompanyProject.Forms
             // 
             // panelContent
             // 
+            this.panelContent.Controls.Add(this.label2);
+            this.panelContent.Controls.Add(this.label1);
             this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelContent.Location = new System.Drawing.Point(145, 0);
             this.panelContent.Name = "panelContent";
             this.panelContent.Size = new System.Drawing.Size(700, 480);
             this.panelContent.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(266, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Курсовой проект";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 22F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(261, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Учет мебели";
             // 
             // MainForm
             // 
@@ -172,6 +201,8 @@ namespace FurnitureSalesCompanyProject.Forms
             this.Text = "Главное меню";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.panelMenuTab.ResumeLayout(false);
+            this.panelContent.ResumeLayout(false);
+            this.panelContent.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -186,5 +217,7 @@ namespace FurnitureSalesCompanyProject.Forms
         private System.Windows.Forms.Button btnOpenCartForm;
         private System.Windows.Forms.Button btnOpenContructsForm;
         private System.Windows.Forms.Button btnOpenContractsHistoryForm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
